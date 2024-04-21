e89ee8dc3a53e5abcb51fa5421fb9274069d87cc
final-developer <lukasgruler@web.de>
2024-04-21 18:25:37 +0200

    extend functionality

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 // MongoDB and Mongoose handling
 const Note = require('./models/note')
 
-// for using json file format automatically when sending/receiving data
-app.use(express.json())
 // for using the /dist folder as frontend
 app.use(express.static('dist'))
+// for using json file format automatically when sending/receiving data
+app.use(express.json())
 
 // "cors" for cross origin resource sharing
 const cors = require('cors')
@@ -27,21 +27,7 @@ app.use(requestLogger)
 
 
 let notes = [
-   {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-   },
-   {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-   },
-   {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-   }
+
 ]
 
 app.get('/', (request, response) => {
@@ -54,54 +40,76 @@ app.get('/api/notes', (request, response) => {
    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-   const id = Number(request.params.id)
-   const note = notes.find(note => note.id === id)
-
-   if (note) {
-      response.json(note)
-   } else {
-      response.status(404).end();
-   }
+app.get('/api/notes/:id', (request, response, next) => {
+   Note.findById(request.params.id)
+      .then(note => {
+         if (note) {
+            response.json(note)
+         } else {
+            response.status(404).end()
+         }
+      })
+      .catch(error => next(error))
 })
 
-const generateId = () => {
-   const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-   return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
    const body = request.body
 
-   if (!body.content) {
-      return response.status(400).json({
-         error: 'content missing'
-      })
-   }
-
-   const note = {
+   const note = new Note({
       content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-   }
+      important: body.important || false
+   })
 
-   notes = notes.concat(note)
-   response.json(note)
+   note.save()
+      .then(savedNote => response.json(savedNote))
+      .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-   const id = Number(request.params.id)
-   notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+   const { content, important } = request.body
 
-   response.status(204).end()
+   Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+   )
+      .then(updatedNote => response.json(updatedNote))
+      .catch(error => next(error))
 })
 
+app.delete('/api/notes/:id', (request, response, next) => {
+   // const id = Number(request.params.id)
+   // notes = notes.filter(note => note.id !== id)
+
+   // response.status(204).end()
+   Note.findByIdAndDelete(request.params.id)
+      .then(result => response.status(204).end())
+      .catch(error => next(error))
+})
+
+
+// unknown endpoint middleware
 const unknownEndpoint = (request, response) => {
    response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+
+// error handler middleware (because of 4 parameters)
+const errorHandler = (error, request, response, next) => {
+   console.error(error.message)
+
+   if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+   } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+   }
+
+   // pass error to default express error handler
+   next(error)
+}
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
