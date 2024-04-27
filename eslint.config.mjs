ee9da38b12c09c0afe34241796b859a7b9a5350e
final-developer <lukasgruler@web.de>
2024-04-27 12:00:23 +0200

    fix lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,22 @@ import stylisticJs from '@stylistic/eslint-plugin-js'
 export default [
   {
     files: ["**/*.js"],
-    languageOptions: { sourceType: "commonjs" },
+    languageOptions: {
+      sourceType: "commonjs"
+    },
     plugins: {
       '@stylistic/js': stylisticJs,
     },
     rules: {
       '@stylistic/js/indent': ['error', 3],
       '@stylistic/js/linebreak-style': ['error', 'windows'],
-      '@stylistic/js/quotes': ['error', 'double'],
+      '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      'eqeqeq': ['error', 'always'],
+      'no-trailing-spaces': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-console': 0
     },
   },
   {
